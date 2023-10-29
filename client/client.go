58a291446b9420d44d566f4e741e555707747cf3
jube <82834149+MidweekAmphibian@users.noreply.github.com>
2023-10-29 19:43:15 +0100

    using message stream for each user to communicate from client to server

---
+++ client/client.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"handin3chitchat/chitchat"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type chatClientStruct struct {
+	stream chitchat.ChatService_JoinClient
+	id     int32
+	name   string
+}
+
+var lamport int32
+
+func main() {
+
+	//Server address
+	const serverAddress = "localhost:5678"
+
+	//We initialize lamport clock
+	lamport = 0
+
+	//we create insecure transport credentials (in the context of this assignment we choose not to worry about security):
+	transportCreds := insecure.NewCredentials()
+	//Establish a grpc connection to the server
+	conn, err := grpc.Dial(serverAddress, grpc.WithTransportCredentials(transportCreds))
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server ... : %v\n", err)
+	}
+	defer conn.Close()
+
+	client := chitchat.NewChatServiceClient(conn)
+	chatClient := chatClientStruct{}
+
+	//create user
+	user := chatClient.CreateUser(client)
+
+	log.Println("Connecting to the gRPC server at ... : " + serverAddress)
+	time.Sleep(time.Millisecond * time.Duration(1000))
+	//Initialize a join stream and set the join stream in chatClient
+	joinStream, err := client.Join(context.Background(), user)
+
+	if err != nil {
+		log.Fatalf("Ouch. Failed to join the chat: %v\n", err)
+	}
+	chatClient.stream = joinStream
+
+	log.Printf("\n\n\n Hello, %s. \n You can disconnect with '/disconnect' \n Write a message ...\n\n", user.Name)
+
+	//We start go routines for sending and recieving messages.
+	go chatClient.SendChatMessage(client)
+	go chatClient.ReceiveMessage(client, user)
+
+	//keep the main function running
+	select {}
+}
+
+func (chatClient *chatClientStruct) SendChatMessage(client chitchat.ChatServiceClient) {
+	for {
+		//read user message from the console.
+		message, err := readUserInput()
+		if err != nil {
+			log.Fatalf("Ouch. Failed to read your chat message from the console: %v ", err)
+		}
+		//We increment lamport in order to give message a lamport timestamp.
+		lamport++
+		//Create new clientMessage and send it to the server.
+		clientMessage := &chitchat.ClientMessage{
+			Name:    chatClient.name,
+			Text:    message,
+			Lamport: lamport,
+		}
+		_, err2 := client.SendMessage(context.Background(), clientMessage)
+		if err2 != nil {
+			log.Fatalf("Failed to send the clientMessage to server: %v\n", err)
+		}
+	}
+}
+
+func (chatClient *chatClientStruct) ReceiveMessage(client chitchat.ChatServiceClient, user *chitchat.User) {
+	for {
+<<<<<<< HEAD
+
+		//recieve a message from the server
+		userStreamServerMessage, err := chatClient.stream.Recv()
+=======
+		message, err := client.BroadcastListener(context.Background(), user)
+		if err != nil {
+			//log.Println("Oh no! Failed to recieve message from server")
+		} else {
+			log.Println(" - ", message.Text)
+		}
+
+		/*//recieve a message from the server
+		serverMessage, err := chatClient.stream.Recv()
+>>>>>>> 805f037ccd99415c430b1876f8bd1249c5f6be42
+		if err != nil {
+			log.Fatalf("Failed to recieve message from server: %v\n", err)
+		}
+		if userStreamServerMessage == nil {
+			log.Fatalf("serverMessage returned nil")
+		}
+
+		//Find lamport timestamp of incoming message, select the highest and increment.
+		incomingLamport := userStreamServerMessage.Lamport
+		lamport = max(lamport, incomingLamport)
+		lamport++
+
+		//Displaying the recieved chat message with lamport time stamp:
+<<<<<<< HEAD
+		log.Printf(" - [%d] %s : %s", lamport, userStreamServerMessage.Name, userStreamServerMessage.Text)
+=======
+		log.Printf("[%d] %s : \n %s", lamport, serverMessage.Name, serverMessage.Text)*/
+>>>>>>> 805f037ccd99415c430b1876f8bd1249c5f6be42
+	}
+}
+
+func (chatClient *chatClientStruct) CreateUser(client chitchat.ChatServiceClient) *chitchat.User {
+	//Generate a random id:
+	randSrc := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(randSrc)
+	id := int32(randGen.Intn(10000)) // Generate a random integer and cast to int32
+	chatClient.id = id
+
+	//Ask client for username:
+	for {
+		fmt.Println("Please enter your username and press 'enter'!")
+		username, err := readUserInput()
+		if err != nil {
+			log.Fatalf("Failed to read username: %v", err)
+			continue //prompt the user to enter username again
+		}
+		chatClient.name = username
+		//break out of the loop since we have a username
+		break
+	}
+
+	//Generate new user:
+	var user = &chitchat.User{
+		Id:      chatClient.id,
+		Name:    chatClient.name,
+		Lamport: lamport,
+	}
+	return user
+}
+func readUserInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	userInput, err := reader.ReadString('\n')
+	//Trim message whitespace from beginning and end.
+	userInput = strings.TrimSpace(userInput)
+	return userInput, err
+
+}
---
+++ server/server.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	chitchat "handin3chitchat/chitchat"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+var lamport int32
+
+type UserStream struct {
+	UserId int32
+	Stream chitchat.ChatService_JoinServer // The gRPC stream
+}
+
+type Server struct {
+	userStreams map[int32]*UserStream
+	chitchat.UnimplementedChatServiceServer
+}
+
+var msgCh = make(chan *chitchat.ClientMessage)
+var userCount = 0
+
+// create map of all userstreams (streans to connected clients)
+var userStreams = make(map[int32]*UserStream)
+
+func main() {
+
+	//initialize lamport clock
+	lamport = 0
+
+	port := "5678" //set the default port to 5678
+
+	//initialize the listener on the specified port. net.Listen listens for incoming connections with tcp socket
+	listen, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Could not listen at port: %s : %v", port, err)
+	}
+	//close listener in case of unexpected exit.
+	defer listen.Close()
+	log.Println("Listening at: " + port)
+
+	//Make instance of grpc server
+	grpcServer := grpc.NewServer()
+	//make instance of chat server structure
+	serverStructure := Server{}
+	//We associate the chat service implementation, represented by the serverStructure structure
+	//with the (new and empty) gRPC server.
+	chitchat.RegisterChatServiceServer(grpcServer, &serverStructure)
+
+	//grpc listen and serve
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		log.Fatalf("Failed to start grpc server: %v", err)
+		log.Print(err)
+	}
+	select {}
+}
+
+var mutex sync.Mutex //We define a mutex to ensure only one client accesses the users map at a time
+//to maintain integrity
+
+func (s *Server) Join(User *chitchat.User, userStream chitchat.ChatService_JoinServer) error {
+
+	//Compare lamport timestamps and select the highest value, then increment to maintain lamport time stamp across chat room.
+	userLamport := User.Lamport
+	//Use mutex to ensure consistency in the lamport timestamp across the server and all connected clients.
+	lamport = max(lamport, userLamport)
+	lamport++
+
+	// Send and broadcast a welcome message
+	welcomeMessage := fmt.Sprintf("Participant %s joined Chitty-Chat at Lamport time %d", User.Name, lamport)
+	message := &chitchat.ClientMessage{
+		Name:    User.Name,
+		Text:    welcomeMessage,
+		Lamport: lamport,
+	}
+	s.SendMessage(context.Background(), message)
+
+	//Add user to map of userstreams.
+	newUserStream := &UserStream{
+		UserId: User.Id,    // Set the user's ID
+		Stream: userStream, // Set the gRPC stream
+	}
+
+	userStreams[User.Id] = newUserStream
+
+	select {}
+}
+
+func (s *Server) SendMessage(ctx context.Context, message *chitchat.ClientMessage) (*chitchat.SentChatResponse, error) {
+	fmt.Println(" - ", message.Lamport, ":", message.Text)
+
+	messageLamport := message.Lamport
+	//Use mutex to ensure consistency in the lamport timestamp across the server and all connected clients.
+	//mutex.Lock()
+	lamport = max(lamport, messageLamport)
+	lamport++
+	//defer mutex.Unlock()
+	message.Lamport = lamport
+
+	//Send the message to all connected users by cycling through the userStreams and sending the message.
+	for _, userStream := range userStreams {
+		if err := userStream.Stream.Send((*chitchat.ServerMessage)(message)); err != nil {
+			log.Fatalf("Failed to send send message to client with id %d", userStream.UserId)
+		}
+	}
+
+	return &chitchat.SentChatResponse{}, nil
+}
+
+//not used anymore
+/*func (s *Server) BroadcastListener(context.Context, *chitchat.User) (*chitchat.ClientMessage, error) {
+	for {
+		clientMessage := <-msgCh
+		// Send the received message to the client
+		return clientMessage, nil
+	}
+}*/
